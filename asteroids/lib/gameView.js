afe9211c565e570f1d45b1abbc31b77dbd157883
App Academy Student <student@appacademy.io>
2015-11-30 17:00:57 -0800

    Finish GameView constructor

--- asteroids/lib/gameView.js
+++ asteroids/lib/gameView.js
@@ -1,3 +1,26 @@
-(function(){
+/* global Asteroids */
 
+(function(){
+  if (typeof Asteroids === "undefined") {
+    window.Asteroids = {};
+  }
+
+  var GameView = Asteroids.GameView = function() {
+    this.game = new Asteroids.Game();
+    var canvasEl = document.getElementsByTagName("canvas")[0];
+    canvasEl.height = window.innerHeight;
+    canvasEl.width = window.innerWidth;
+
+    this.ctx = canvasEl.getContext("2d");
+  };
+
+  GameView.prototype.start = function() {
+    var game = this.game;
+    var ctx = this.ctx;
+
+    setInterval(function(){
+      game.moveObjects();
+      game.draw(ctx);
+    }, 20);
+  };
 })();
